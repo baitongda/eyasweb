870ec432e779240338d4c819e52d960285efbbde
Eyas <liuyuesongde@163.com>
2016-02-20 15:44:41 +0800

    Signed-off-by: Eyas <liuyuesongde@163.com>

--- app/client/router.js
+++ app/client/router.js
@@ -3,7 +3,7 @@ import {Router, Route, IndexRoute, browserHistory} from 'react-router';
 import history  from 'utils/history';
 import Framework from 'fw';
 import HomeRouter from 'home/router';
-// import AdminRouter from 'admin/router';
+import ExampleRouter from 'example/router';
 // import AuthRouter from 'auth/router';
 import Index from 'home';
 
@@ -12,6 +12,7 @@ const APPRouter = (
     <Route path="/" component={Framework}>
       <IndexRoute component={Index} />
       {HomeRouter}
+      {ExampleRouter}
     </Route>
   </Router>
 );
