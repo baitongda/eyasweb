c4eefcf7536667886212233263466fad072b5c85
Eyas <liuyuesongde@163.com>
2016-02-24 23:35:13 +0800

    init post api

---
+++ app/server/api/models/Post.js
@@ -0,0 +1,46 @@
+/**
+ * Post.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+    title: {
+      type: 'string',
+      required: true,
+    },
+    author: {
+      model: 'user',
+      defaultsTo: 1
+    },
+    expect: {
+      type: 'text',
+      defaultsTo: '',
+    }
+    content: {
+      type: 'text',
+      defaultsTo: ''
+    },
+    status: {
+      type: 'string',
+      defaultsTo: 'active'
+    },
+    slug: {
+      type: 'string',
+      defaultsTo: ''
+    },
+    categorys: {
+      type: 'array',
+      defaultsTo: []
+    },
+    tags: {
+      type: 'array',
+      defaultsTo: []
+    }
+
+  }
+};
+
