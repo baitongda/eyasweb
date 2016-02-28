dce363b5e9b48430c66e665c69db898c31bda47d
Eyas <liuyuesongde@163.com>
2016-02-28 23:55:21 +0800

    登录后改变导航栏

--- app/client/modules/auth/actions/index.js
+++ app/client/modules/auth/actions/index.js
@@ -1,13 +1,25 @@
 const constant = Constant('auth');
 
 export function login(formdata){
-  return dispatch => {
+  return (dispatch, getState) => {
     request.post(config.server + '/login')
       .send(formdata)
       .end((err, res) => {
         const data = res.body;
         if(data.token){
           sessionStorage.setItem('auth', data.token);
+          // 修改导航栏
+          const navbar = getState().navbar;
+          _.remove(navbar.data, item => item.link === '/auth');
+          navbar.data.push({
+            title: '管理面板',
+            link: '/admin'
+          })
+          dispatch({
+            type: Constant('navbar').ChangeNavbar,
+            data: navbar.data
+          })
+          // 登录成功
           dispatch({
             type: constant.Login,
             data
--- app/client/modules/framework/reducers/navbar.js
+++ app/client/modules/framework/reducers/navbar.js
@@ -1,3 +1,4 @@
+const constant = Constant('navbar');
 const initState = {
   data: [{
     title: '首页',
@@ -5,21 +6,35 @@ const initState = {
   }, {
     title: '博客',
     link: '/blog'
-  }, {
-    title: 'example',
-    link: '/example'
-  }, {
-    title: 'Admin',
-    link: '/admin'
-  }, {
+  }, 
+  // {
+  //   title: 'example',
+  //   link: '/example'
+  // }, 
+  // {
+  //   title: 'Admin',
+  //   link: '/admin'
+  // }, 
+  {
     title: 'Login',
     link: '/auth'
-  }, {
-    title: '关于我',
-    link: '/about'
-  }]
+  }, 
+  // {
+  //   title: '关于我',
+  //   link: '/about'
+  // }
+  ]
 };
 
 export default function navbar(state = initState, action){
+  switch(action.type){
+    case constant.of('ChangeNavbar'):
+      return {
+        ...state,
+        data: [
+          ...action.data
+        ]
+      };
+  }
   return state;
 }
