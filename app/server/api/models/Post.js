5c1aadd41170e6c9618608684cea54351dd99a63
Eyas <liuyuesongde@163.com>
2016-03-02 23:51:38 +0800

    保存文件前先转化一次markdown

--- app/server/api/models/Post.js
+++ app/server/api/models/Post.js
@@ -4,6 +4,7 @@
  * @description :: TODO: You might write a short summary of how this model works and what it represents here.
  * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
  */
+import toMarkdown from 'to-markdown';
 
 module.exports = {
 
@@ -40,7 +41,10 @@ module.exports = {
       collection: 'tags',
       via: 'tag'
     }
-
+  },
+  beforeCreate(val, cb){
+    val.content = toMarkdown(val.content);
+    cb();
   }
 };
 
