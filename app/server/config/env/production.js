6a338a3d39c6395b5994926463e6a0dc13973e27
Eyas <liuyuesongde@163.com>
2016-03-09 22:13:59 +0800

    Signed-off-by: Eyas <liuyuesongde@163.com>

--- app/server/config/env/production.js
+++ app/server/config/env/production.js
@@ -18,7 +18,7 @@ module.exports = {
    ***************************************************************************/
 
   models: {
-    connection: 'DaoMongodbServer',
+    connection: 'sqliteProduction',
     migrate: 'safe'
   },
 
