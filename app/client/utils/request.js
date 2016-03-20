6f61bf41776072e46b2caa55367eee938f058be2
Eyas <liuyuesongde@163.com>
2016-03-20 22:38:11 +0800

    Signed-off-by: Eyas <liuyuesongde@163.com>

--- app/client/utils/request.js
+++ app/client/utils/request.js
@@ -1,11 +1,10 @@
 import superagent from 'superagent-defaults';
 
 const request = superagent();
-request.set('authorization', 'JWT ' + (sessionStorage.getItem('auth') || 'none'));
-
 request.reloadJwt = () => {
   request.set('authorization', 'JWT ' + (sessionStorage.getItem('auth') || 'none'));
   window.request = request;
 };
+request.reloadJwt();
 
 module.exports = request;
